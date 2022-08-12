2dbdafad6fab83549857ee6fb3a926b8842a28c5
Adil Zuhri <106576506+adilzuhri@users.noreply.github.com>
2022-08-12 09:17:45 +0800

    master branch

---
+++ src/test/java/VTTP/nus/iss/sg/SSFAssessment/SsfAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package VTTP.nus.iss.sg.SSFAssessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsfAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
